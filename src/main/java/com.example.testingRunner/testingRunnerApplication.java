fb26ab7cb42bbc9a4205031d2a23c14e1ff4f60c
KevinEjiofor <ejioforkelvin@gmail.com>
2024-06-11 14:11:12 +0100

    Initial commit

---
+++ src/main/java/com.example.testingRunner/testingRunnerApplication.java
@@ -0,0 +1,12 @@
+package com.example.testingRunner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class testingRunnerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(testingRunnerApplication.class, args);
+	}
+}
